d70ca1007bb76cc99b4278079627d0dda08ceefe
zhangshaokai <1054764349@qq.com>
2018-03-10 15:42:32 +0800

    矩形面积的计算

--- node01/node01.js
+++ node01/node01.js
@@ -2,12 +2,12 @@ window.onload=function(){
     var wid = document.getElementById('wid');
     var hei = document.getElementById('hei');
     var btn = document.getElementById('btn');
+    var z = document.getElementById('z');
+    var area = document.getElementById('area');
     btn.onclick=function(){
         var width=wid.value;
         var height=hei.value;
-        z=width*2+height*2;
-        area = width*height;
-        alert(z);
-        alert(area);
+        z.value=width*2+height*2;
+        area.value = width*height;
     }
 }
